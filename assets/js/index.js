f8a30be789a115193876e4342f84baf8a593b066
Svetlana <sveselivanova@gmail.com>
2024-04-28 23:42:41 -0400

    Created english version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+const myCarouselElement = document.querySelector("#carouselViajes");
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+});
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
