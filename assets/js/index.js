a520f94e070d2ef229251b47c60fff4a4c079a22
Svetlana <sveselivanova@gmail.com>
2024-07-19 22:48:16 -0400

    updated mobile view

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,5 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
-const myCarouselElement = document.querySelector("#carouselViajes");
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false,
-});
-
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
